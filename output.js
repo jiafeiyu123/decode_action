3f3fead8272d50b5c6192ab0a8ddf34e0efacb49
action <action@github.com>
2025-04-06 09:11:48 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Apr 06 2025 09:10:40 GMT+0000 (Coordinated Universal Time)
+//Sun Apr 06 2025 09:11:48 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("阿维塔");
